764b75900176f1dfde442dba3d923f0c07c20148
Takuya_Nakahara <yky2800.rthm.55tn@gmail.com>
2018-11-25 04:52:27 +0000

    Gemfileのrubyバージョン変更、Cloud9用にdatabase.ymlにtamplate0を追加

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,4 @@ ActiveRecord::Schema.define(version: 2018_11_16_123817) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "places", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
